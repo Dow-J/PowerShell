467ab5b0daf37b9f23b1d9b88c4c57a3e84c0623
Joshua Dow <dow.c.joshua@gmail.com>
2017-03-02 11:27:02 -0500

    Script to query AD against a list of users and export matches to .csv

---
+++ ADLookUp.ps1
@@ -0,0 +1,14 @@
+﻿import-module activedirectory
+$Matches = @()
+Import-csv -Path C:\scripts\testcsv2.csv | 
+% { 
+    $ADUser = get-aduser -Filter "UserPrincipalName -eq '$($_.InternetAddress)'" -Properties * 
+    
+    if ($_.InternetAddress -eq $ADUser.userprincipalname) {
+        $Matches += [PSCustomObject]@{
+            Userprincipalname = $ADUser.userprincipalname
+            #you can add more column headers if you want
+        }
+    }
+    $Matches | Export-Csv -Path "matches.csv" -NoTypeInformation
+}   
