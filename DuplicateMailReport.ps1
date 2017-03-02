84663936ad8dc746c5b542dff2f8667e79f5a13c
Joshua Dow <dow.c.joshua@gmail.com>
2017-03-02 11:24:43 -0500

    Script for querying AD for duplicate attributes using a hash table. Currently configured to query mail attribute but can be modified for any.

---
+++ DuplicateMailReport.ps1
@@ -0,0 +1,6 @@
+﻿import-module activedirectory # import ad powershell module
+$hash = @{}; # create empty hash table
+$users = Get-ADUser -Filter { Enabled -eq $true } -Properties Mail # Gets AD Users that are enabled and stores the value of their mail attribute
+$users | ForEach-Object { if($_.Mail) { $hash[$_.Mail] += 1 } } # Go through all filtered users and, if mail attribute is not null, tally each occurance of mail attribute
+$filteredUsers = $users | Where-Object { if($_.Mail) { $hash[$_.Mail] -gt 1 } } # Gets all users with a tally greater than 1
+$filteredUsers | Select-Object -Property SamAccountName, Mail | Export-Csv -Path "MailDup.csv" -NoTypeInformation
